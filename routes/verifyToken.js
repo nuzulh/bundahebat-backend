69c383c5b7082e3a80223cf5092e8c970fd80bd2
nuzulh <nuzulhikam0@gmail.com>
2022-05-09 04:03:05 +0700

    register & login auth

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -5,7 +5,7 @@ const auth = (req, res, next) => {
     if (!token) return res.status(401).send('Access Denied');
 
     try {
-        const verified = jwt.verify(token. process.env.TOKEN_SECRET);
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
     } catch(err) {
         res.status(400).send('Invalid Token');
